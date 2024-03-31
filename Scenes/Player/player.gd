0edc50f8c3ff0035889261dd6e9d4ad7774247a2
Emily Krohn <emilykrohn2@gmail.com>
2024-03-31 08:20:02 -0700

    Tile map moves down when the player jumps

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -3,7 +3,7 @@ extends Entity
 const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var tilemap = get_node("../TileMap")
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -11,6 +11,7 @@ func _physics_process(delta):
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		tilemap.position.y += 10
 
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
