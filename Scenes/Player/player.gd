8ee02dd7002566f0b85c5818f1b3bb0f6b282d5e
Emily Krohn <emilykrohn2@gmail.com>
2024-03-31 08:50:45 -0700

    Prevent player from being pushed upward
    When the tile map was moving up to go back to the player, the player was being pushed up when the player need to stay in the center of the screen for the entire game

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -2,10 +2,16 @@ extends Entity
 
 const JUMP_VELOCITY = -400.0
 var is_jumping = false
+var screen_center_y = 0
 
 @onready var tilemap = get_node("../TileMap")
 
+func _ready():
+	var screen_height = get_viewport_rect().size.y
+	screen_center_y = screen_height / 2
+
 func _physics_process(delta):
+	global_position.y = screen_center_y
 	if not is_on_floor():
 		tilemap.position.y -= 150 * delta
 
